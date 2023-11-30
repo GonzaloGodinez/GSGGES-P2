9e2f44b9c3a37d3f6152017e50c49f99fe32a6fd
gmoney3303 <garrettmsheppard@yahoo.com>
2023-11-29 21:01:35 -0700

    fixed npm start few naming errors

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoute');
+// const postRoutes = require('./postRoute');
 const commentRoutes = require('./commentRoute');
+const bookRoutes = require('./bookRoute.js')
 
+router.use('/book', bookRoutes);
 router.use('/users', userRoutes);
-router.use('/post', postRoutes);
+// router.use('/post', postRoutes);
 router.use('/comment', commentRoutes);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const User = require('./User');
-const Book = require('./Book');
+const Book = require('./books');
 
 User.hasMany(Book, {
   foreignKey: 'id',
