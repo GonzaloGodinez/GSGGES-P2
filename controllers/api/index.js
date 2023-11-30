a24678d9f5f3fd4d61e1ab956206d7af5c3e8c53
GonzaloGodinez <Gonzalo.Godinez@comcast.net>
2023-11-29 21:15:01 -0700

    added routers user and userbooks homeroutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoute');
-const commentRoutes = require('./commentRoute');
+const postRoutes = require('./bookRoute');
+const commentRoutes = require('./userbookRoute');
 
 router.use('/users', userRoutes);
-router.use('/post', postRoutes);
-router.use('/comment', commentRoutes);
+router.use('/book', bookRoutes);
+router.use('/userbook', userbookRoute);
 
 module.exports = router;
