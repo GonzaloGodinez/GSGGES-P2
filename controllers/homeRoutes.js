83b5993cc175dd3134ce038ff8cc9ab50d3701c9
GonzaloGodinez <Gonzalo.Godinez@comcast.net>
2023-11-30 19:48:58 -0700

    clean index home routeschema  file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Book, User, Book } = require('../models');
+const { Book, User, userBooks } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
