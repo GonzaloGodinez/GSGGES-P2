9fe0452798f8400a202c4add8b36de48f002075e
GonzaloGodinez <Gonzalo.Godinez@comcast.net>
2023-11-30 20:51:58 -0700

    staging

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,20 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const postRoutes = require('./bookRoute');
-const commentRoutes = require('./userbookRoute');
-
-// const postRoutes = require('./postRoute');
-const commentRoutes = require('./commentRoute');
+const reviewRoute = require('./reviewRoute');
 const bookRoutes = require('./bookRoute.js')
-
 
 router.use('/book', bookRoutes);
 router.use('/users', userRoutes);
-router.use('/book', bookRoutes);
-router.use('/userbook', userbookRoute);
-
-// router.use('/post', postRoutes);
-router.use('/comment', commentRoutes);
-
+router.use('/userbook', reviewRoute);
 
 module.exports = router;
