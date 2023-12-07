e98b6f355d4e33b81f0c75af86fb9463b589ea10
gmoney3303 <garrettmsheppard@yahoo.com>
2023-12-06 20:34:02 -0700

    testing for add

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,50 +1,4 @@
-const newFormHandler = async (event) => {
-  event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
-
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
-      method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to create project');
-    }
-  }
-};
-
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/projects/${id}`, {
-      method: 'DELETE',
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to delete project');
-    }
-  }
-};
-
-document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
-
-document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
 
   function dropMenu() {
     document.getElementById("dropdown").classList.toggle("show");
@@ -96,33 +50,47 @@ document
     });
   });
   
-  // Handle click on the "Add" button for each book
-  document.querySelectorAll('.add-book').forEach(button => {
-    button.addEventListener('click', async function() {
-      const bookId = this.getAttribute('data-id');
+
+  function getUserIdFromSession() {
+    // Logic to retrieve the user ID from the session
+    // For example:
+    return sessionStorage.getItem('userId'); // Retrieving the user ID stored in session storage
+  }
   
+  const userId = getUserIdFromSession(); // Function to get the user ID from the session
+
+// Handle click on the "Add" button for each book
+document.querySelectorAll('.add-book').forEach(button => {
+  button.addEventListener('click', async function() {
+    const bookId = this.getAttribute('data-id');
+     const bookDetails = books.find(book => book.id === parseInt(bookId));
+
+    if (bookDetails) {
       try {
         const response = await fetch(`/api/users/add-book/${bookId}`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
-          // If required, include additional data in the body
-          // For example, user ID or any other relevant information
           body: JSON.stringify({
-            // Include any necessary data for the request
+            userId: userId, 
+            bookId: bookId,
+            title: bookDetails.title,
+            author: bookDetails.author,
+            genre: bookDetails.genre
           })
         });
-  
+
         if (response.ok) {
           alert('Book added to your list!');
         } else {
-          // Handle the case where the addition failed
           alert('Failed to add book to your list.');
         }
       } catch (error) {
-        // Handle any network errors or exceptions
         console.error('Error adding book:', error);
       }
-    });
+    } else {
+      console.error('Book details not found.');
+    }
   });
+});
