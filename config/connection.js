f9db7130c4519a9a32ec1ade9d6f572fecdec725
GonzaloGodinez <Gonzalo.Godinez@comcast.net>
2023-12-11 18:04:35 -0700

    Added proc

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,7 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
+  sequelize = new Sequelize(process.env.JAWSDB_URL)code .
     process.env.DB_PASSWORD,
     {
       host: 'localhost',
